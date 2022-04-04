87b1823c1967f646b618af9dd3bd92905ce66341
llau08 <llau251@gmail.com>
2022-04-04 16:40:41 -0700

    updated about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,14 +5,7 @@ function About() {
     <section id="about">
       <h3 className="about__title">About Me</h3>
       <p className="about__txt">
-        I made a career pivot after spending 5 years as an Insurance Broker. One
-        of my best friends had introduced me to coding and I enjoyed it
-        instantly. I love it for the challenge it brings and the creativity
-        surrounding it. I'm not someone who has a university degree, however I
-        am a proud BrainStation bootcamp graduate that strives to learn and
-        showcase my best work always. My hobbies when I'm not practicing code
-        include watching movies/shows, playing video games, and hanging out with
-        my friends.
+        I started my coding journey at 23. One of my best friends had introduced the idea to me and we took a part-time course together at BrainStation. I enjoyed the challenge and surrounding creativity. The mentorship was also very inspiring that I decided to make a career pivot from being an Insurance Broker. I proudly graduated from the 12-week intensive Web Development bootcamp at BrainStation as well. Since then, I've strived to build projects and learn different languages to further explore my new career and showcase my best work. When I'm not practicing code, I love to stream movies and shows, play video games and hang out with friends. Feel free to contact me for collaboration or answer questions about any of my work.
       </p>
     </section>
   );
