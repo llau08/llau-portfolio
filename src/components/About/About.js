16c9d72902cc2ce0b2fbedac1d590804ba070cb5
llau08 <llau251@gmail.com>
2022-04-04 16:44:12 -0700

    updated about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,17 @@ function About() {
     <section id="about">
       <h3 className="about__title">About Me</h3>
       <p className="about__txt">
-        I started my coding journey at 23. One of my best friends had introduced the idea to me and we took a part-time course together at BrainStation. I enjoyed the challenge and surrounding creativity. The mentorship was also very inspiring that I decided to make a career pivot from being an Insurance Broker. I proudly graduated from the 12-week intensive Web Development bootcamp at BrainStation as well. Since then, I've strived to build projects and learn different languages to further explore my new career and showcase my best work. When I'm not practicing code, I love to stream movies and shows, play video games and hang out with friends. Feel free to contact me for collaboration or answer questions about any of my work.
+        I started my coding journey at 23. One of my best friends had introduced
+        the idea to me and we took a part-time course together at BrainStation.
+        I enjoyed the challenge and surrounding creativity. The mentorship was
+        also very inspiring that I decided to make a career pivot from being an
+        Insurance Broker. I proudly graduated from the 12-week intensive Web
+        Development bootcamp at BrainStation as well. Since then, I've strived
+        to build projects and learn different languages to further explore my
+        new career and showcase my best work. When I'm not practicing code, I
+        love to stream movies and shows, play video games and hang out with
+        friends. Feel free to contact me for collaboration or answer questions
+        about any of my work.
       </p>
     </section>
   );
